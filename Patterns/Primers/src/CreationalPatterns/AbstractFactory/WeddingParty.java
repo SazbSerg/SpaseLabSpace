3dc8b740716bb645657fccdde8797c69e715c7fd
SazbSerg <sazb.serg@gmail.com>
2021-07-17 15:03:09 +0300

    commit 17/01/21

--- Patterns/Primers/src/CreationalPatterns/AbstractFactory/WeddingParty.java
+++ Patterns/Primers/src/CreationalPatterns/AbstractFactory/WeddingParty.java
@@ -5,13 +5,13 @@ import CreationalPatterns.AbstractFactory.Wedding.WeddingTeam;
 
 public class WeddingParty {
     public static void main(String[] args) {
-    EventTeamFactory eventTeamFactory = new WeddingTeam();
-    Managering manager = eventTeamFactory.getManager();
-    Cooking cook = eventTeamFactory.getCook();
-    Waitering waiter = eventTeamFactory.getWaiter();
+        EventTeamFactory eventTeamFactory = new WeddingTeam(); //создание новой команды мероприятия: свадьба, др или конф.
+        Managering manager = eventTeamFactory.getManager();
+        Cooking cook = eventTeamFactory.getCook();
+        Waitering waiter = eventTeamFactory.getWaiter();
 
-    manager.manageTeam();
-    cook.toCook();
-    waiter.postOrders();
-    }
+        manager.manageTeam();
+        cook.toCook();
+        waiter.postOrders();
+        }
 }
